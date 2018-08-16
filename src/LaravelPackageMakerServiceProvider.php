c07045a5b6912fa79b09fc5f2f04534447832731
Naoray <krishan.koenig@googlemail.com>
2018-08-16 08:40:20 +0200

    add Mail & and Job Commands

--- src/LaravelPackageMakerServiceProvider.php
+++ src/LaravelPackageMakerServiceProvider.php
@@ -4,17 +4,19 @@ namespace Naoray\LaravelPackageMaker;
 
 use Illuminate\Support\ServiceProvider;
 use Naoray\LaravelPackageMaker\Commands\AddPackage;
+use Naoray\LaravelPackageMaker\Commands\JobMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\TestMakeCommand;
-use Naoray\LaravelPackageMaker\Commands\ReadmeMakeCommand;
+use Naoray\LaravelPackageMaker\Commands\MailMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\TravisMakeCommand;
+use Naoray\LaravelPackageMaker\Commands\ReadmeMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\CodecovMakeCommand;
-use Naoray\LaravelPackageMaker\Commands\LicenseMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\PackageMakeCommand;
-use Naoray\LaravelPackageMaker\Commands\PhpunitMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\StyleciMakeCommand;
+use Naoray\LaravelPackageMaker\Commands\LicenseMakeCommand;
+use Naoray\LaravelPackageMaker\Commands\PhpunitMakeCommand;
+use Naoray\LaravelPackageMaker\Commands\ProviderMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\BaseTestMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\ComposerMakeCommand;
-use Naoray\LaravelPackageMaker\Commands\ProviderMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\GitignoreMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\ContributionMakeCommand;
 
@@ -29,17 +31,19 @@ class LaravelPackageMakerServiceProvider extends ServiceProvider
     {
         $this->commands([
             AddPackage::class,
+            JobMakeCommand::class,
             TestMakeCommand::class,
-            TravisMakeCommand::class,
+            MailMakeCommand::class,
             ReadmeMakeCommand::class,
+            TravisMakeCommand::class,
             CodecovMakeCommand::class,
-            StyleciMakeCommand::class,
             LicenseMakeCommand::class,
             PhpunitMakeCommand::class,
+            StyleciMakeCommand::class,
             PackageMakeCommand::class,
+            ComposerMakeCommand::class,
             ProviderMakeCommand::class,
             BaseTestMakeCommand::class,
-            ComposerMakeCommand::class,
             GitignoreMakeCommand::class,
             ContributionMakeCommand::class,
         ]);
