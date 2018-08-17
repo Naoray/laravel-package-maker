8a80583ecd5a6facaf84fa9422d92f740251cafd
Naoray Kroaths <krishan.koenig@gmail.com>
2018-08-17 07:47:51 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Commands/Routing/ControllerMakeCommand.php
+++ src/Commands/Routing/ControllerMakeCommand.php
@@ -24,80 +24,80 @@ class ControllerMakeCommand extends MakeController
      */
     protected function resolveDirectory()
     {
-        return $this->getDirInput() . 'src';
-	}
+        return $this->getDirInput().'src';
+    }
 
-	/**
-	 * Build the replacements for a parent controller.
-	 *
-	 * @return array
-	 */
-	protected function buildParentReplacements()
-	{
-		$parentModelClass = $this->parseModel($this->option('parent'));
+    /**
+     * Build the replacements for a parent controller.
+     *
+     * @return array
+     */
+    protected function buildParentReplacements()
+    {
+        $parentModelClass = $this->parseModel($this->option('parent'));
 
-		if (!class_exists($parentModelClass)) {
-			if ($this->confirm("A {$parentModelClass} model does not exist. Do you want to generate it?", true)) {
-				$this->call('make:package:model', [
-					'name' => $parentModelClass,
-					'--namespace' => $this->rootNamespace(),
-					'--dir' => $this->basePath(),
-				]);
-			}
-		}
+        if (! class_exists($parentModelClass)) {
+            if ($this->confirm("A {$parentModelClass} model does not exist. Do you want to generate it?", true)) {
+                $this->call('make:package:model', [
+                    'name' => $parentModelClass,
+                    '--namespace' => $this->rootNamespace(),
+                    '--dir' => $this->basePath(),
+                ]);
+            }
+        }
 
-		return [
-			'ParentDummyFullModelClass' => $parentModelClass,
-			'ParentDummyModelClass' => class_basename($parentModelClass),
-			'ParentDummyModelVariable' => lcfirst(class_basename($parentModelClass)),
-		];
-	}
+        return [
+            'ParentDummyFullModelClass' => $parentModelClass,
+            'ParentDummyModelClass' => class_basename($parentModelClass),
+            'ParentDummyModelVariable' => lcfirst(class_basename($parentModelClass)),
+        ];
+    }
 
-	/**
-	 * Build the model replacement values.
-	 *
-	 * @param  array  $replace
-	 * @return array
-	 */
-	protected function buildModelReplacements(array $replace)
-	{
-		$modelClass = $this->parseModel($this->option('model'));
+    /**
+     * Build the model replacement values.
+     *
+     * @param  array  $replace
+     * @return array
+     */
+    protected function buildModelReplacements(array $replace)
+    {
+        $modelClass = $this->parseModel($this->option('model'));
 
-		if (!class_exists($modelClass)) {
-			if ($this->confirm("A {$modelClass} model does not exist. Do you want to generate it?", true)) {
-				$this->call('make:package:model', [
-					'name' => $modelClass,
-					'--namespace' => $this->rootNamespace(),
-					'--dir' => $this->basePath(),
-				]);
-			}
-		}
+        if (! class_exists($modelClass)) {
+            if ($this->confirm("A {$modelClass} model does not exist. Do you want to generate it?", true)) {
+                $this->call('make:package:model', [
+                    'name' => $modelClass,
+                    '--namespace' => $this->rootNamespace(),
+                    '--dir' => $this->basePath(),
+                ]);
+            }
+        }
 
-		return array_merge($replace, [
-			'DummyFullModelClass' => $modelClass,
-			'DummyModelClass' => class_basename($modelClass),
-			'DummyModelVariable' => lcfirst(class_basename($modelClass)),
-		]);
-	}
+        return array_merge($replace, [
+            'DummyFullModelClass' => $modelClass,
+            'DummyModelClass' => class_basename($modelClass),
+            'DummyModelVariable' => lcfirst(class_basename($modelClass)),
+        ]);
+    }
 
-	/**
-	 * Get the fully-qualified model class name.
-	 *
-	 * @param  string  $model
-	 * @return string
-	 */
-	protected function parseModel($model)
-	{
-		if (preg_match('([^A-Za-z0-9_/\\\\])', $model)) {
-			throw new InvalidArgumentException('Model name contains invalid characters.');
-		}
+    /**
+     * Get the fully-qualified model class name.
+     *
+     * @param  string  $model
+     * @return string
+     */
+    protected function parseModel($model)
+    {
+        if (preg_match('([^A-Za-z0-9_/\\\\])', $model)) {
+            throw new InvalidArgumentException('Model name contains invalid characters.');
+        }
 
-		$model = trim(str_replace('/', '\\', $model), '\\');
+        $model = trim(str_replace('/', '\\', $model), '\\');
 
-		if (!starts_with($model, $rootNamespace = $this->rootNamespace()) && !starts_with($model, $this->laravel->getNamespace())) {
-			$model = $rootNamespace . '\\' . $model;
-		}
+        if (! starts_with($model, $rootNamespace = $this->rootNamespace()) && ! starts_with($model, $this->laravel->getNamespace())) {
+            $model = $rootNamespace.'\\'.$model;
+        }
 
-		return $model;
-	}
+        return $model;
+    }
 }
