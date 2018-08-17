9a42a3047016251864ccfb8cfcf5ade6b4be61a5
Naoray Kroaths <krishan.koenig@gmail.com>
2018-08-17 07:44:39 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Commands/Foundation/PolicyMakeCommand.php
+++ src/Commands/Foundation/PolicyMakeCommand.php
@@ -2,71 +2,70 @@
 
 namespace Naoray\LaravelPackageMaker\Commands\Foundation;
 
-use Symfony\Component\Console\Input\InputOption;
 use Naoray\LaravelPackageMaker\Traits\HasNameAttribute;
 use Naoray\LaravelPackageMaker\Traits\CreatesPackageStubs;
 use Illuminate\Foundation\Console\PolicyMakeCommand as MakePolicy;
 
 class PolicyMakeCommand extends MakePolicy
 {
-	use CreatesPackageStubs, HasNameAttribute;
+    use CreatesPackageStubs, HasNameAttribute;
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'make:package:policy';
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:package:policy';
 
-	/**
-	 * Get the destination class path.
-	 *
-	 * @return string
-	 */
-	protected function resolveDirectory()
-	{
-		return $this->getDirInput() . '/src';
-	}
+    /**
+     * Get the destination class path.
+     *
+     * @return string
+     */
+    protected function resolveDirectory()
+    {
+        return $this->getDirInput().'/src';
+    }
 
-	/**
-	 * Replace the model for the given stub.
-	 *
-	 * @param  string  $stub
-	 * @param  string  $model
-	 * @return string
-	 */
-	protected function replaceModel($stub, $model)
-	{
-		$model = str_replace('/', '\\', $model);
+    /**
+     * Replace the model for the given stub.
+     *
+     * @param  string  $stub
+     * @param  string  $model
+     * @return string
+     */
+    protected function replaceModel($stub, $model)
+    {
+        $model = str_replace('/', '\\', $model);
 
-		$namespaceModel = $this->rootNamespace() . '\\' . $model;
+        $namespaceModel = $this->rootNamespace().'\\'.$model;
 
-		if (starts_with($model, '\\')) {
-			$stub = str_replace('NamespacedDummyModel', trim($model, '\\'), $stub);
-		} else {
-			$stub = str_replace('NamespacedDummyModel', $namespaceModel, $stub);
-		}
+        if (starts_with($model, '\\')) {
+            $stub = str_replace('NamespacedDummyModel', trim($model, '\\'), $stub);
+        } else {
+            $stub = str_replace('NamespacedDummyModel', $namespaceModel, $stub);
+        }
 
-		$stub = str_replace(
-			"use {$namespaceModel};\nuse {$namespaceModel};",
-			"use {$namespaceModel};",
-			$stub
-		);
+        $stub = str_replace(
+            "use {$namespaceModel};\nuse {$namespaceModel};",
+            "use {$namespaceModel};",
+            $stub
+        );
 
-		$model = class_basename(trim($model, '\\'));
+        $model = class_basename(trim($model, '\\'));
 
-		$dummyUser = class_basename(config('auth.providers.users.model'));
+        $dummyUser = class_basename(config('auth.providers.users.model'));
 
-		$dummyModel = camel_case($model) === 'user' ? 'model' : $model;
+        $dummyModel = camel_case($model) === 'user' ? 'model' : $model;
 
-		$stub = str_replace('DocDummyModel', snake_case($dummyModel, ' '), $stub);
+        $stub = str_replace('DocDummyModel', snake_case($dummyModel, ' '), $stub);
 
-		$stub = str_replace('DummyModel', $model, $stub);
+        $stub = str_replace('DummyModel', $model, $stub);
 
-		$stub = str_replace('dummyModel', camel_case($dummyModel), $stub);
+        $stub = str_replace('dummyModel', camel_case($dummyModel), $stub);
 
-		$stub = str_replace('DummyUser', $dummyUser, $stub);
+        $stub = str_replace('DummyUser', $dummyUser, $stub);
 
-		return str_replace('DocDummyPluralModel', snake_case(str_plural($dummyModel), ' '), $stub);
-	}
+        return str_replace('DocDummyPluralModel', snake_case(str_plural($dummyModel), ' '), $stub);
+    }
 }
