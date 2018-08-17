9046ce47ee78b6a68fbf677483e85d39254c713e
Naoray <krishan.koenig@googlemail.com>
2018-08-17 09:36:22 +0200

    add Event command

---
+++ src/Commands/Foundation/EventMakeCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Naoray\LaravelPackageMaker\Commands\Foundation;
+
+use Naoray\LaravelPackageMaker\Traits\HasNameAttribute;
+use Naoray\LaravelPackageMaker\Traits\CreatesPackageStubs;
+use Illuminate\Foundation\Console\EventMakeCommand as MakeEvent;
+
+class EventMakeCommand extends MakeEvent
+{
+    use CreatesPackageStubs, HasNameAttribute;
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:package:event';
+
+    /**
+     * Get the destination class path.
+     *
+     * @return string
+     */
+    protected function resolveDirectory()
+    {
+        return $this->getDirInput() . '/src';
+    }
+}
