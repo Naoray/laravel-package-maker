3da37bdcf201ce3b3e2b8fc80444608f89c9b0bd
Naoray <krishan.koenig@googlemail.com>
2018-08-19 18:20:16 +0200

    add SetPackageCredentials, DeletePackageCredentials & reorganize provider

--- src/LaravelPackageMakerServiceProvider.php
+++ src/LaravelPackageMakerServiceProvider.php
@@ -5,23 +5,25 @@ namespace Naoray\LaravelPackageMaker;
 use Illuminate\Support\ServiceProvider;
 use Naoray\LaravelPackageMaker\Commands\AddPackage;
 use Naoray\LaravelPackageMaker\Commands\PackageMakeCommand;
-use Naoray\LaravelPackageMaker\Commands\Foundation\JobMakeCommand;
-use Naoray\LaravelPackageMaker\Commands\Package\ReadmeMakeCommand;
+use Naoray\LaravelPackageMaker\Commands\SetPackageCredentials;
+use Naoray\LaravelPackageMaker\Commands\DeletePackageCredentials;
 use Naoray\LaravelPackageMaker\Commands\Package\TravisMakeCommand;
-use Naoray\LaravelPackageMaker\Commands\Foundation\MailMakeCommand;
-use Naoray\LaravelPackageMaker\Commands\Foundation\RuleMakeCommand;
-use Naoray\LaravelPackageMaker\Commands\Foundation\TestMakeCommand;
+use Naoray\LaravelPackageMaker\Commands\Package\ReadmeMakeCommand;
+use Naoray\LaravelPackageMaker\Commands\Foundation\JobMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\Package\CodecovMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\Package\LicenseMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\Package\PhpunitMakeCommand;
+use Naoray\LaravelPackageMaker\Commands\Foundation\RuleMakeCommand;
+use Naoray\LaravelPackageMaker\Commands\Foundation\TestMakeCommand;
+use Naoray\LaravelPackageMaker\Commands\Foundation\MailMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\Package\StyleciMakeCommand;
-use Naoray\LaravelPackageMaker\Commands\Database\FactoryMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\Foundation\EventMakeCommand;
-use Naoray\LaravelPackageMaker\Commands\Foundation\ModelMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\Package\BaseTestMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\Package\ComposerMakeCommand;
-use Naoray\LaravelPackageMaker\Commands\Foundation\PolicyMakeCommand;
+use Naoray\LaravelPackageMaker\Commands\Database\FactoryMakeCommand;
+use Naoray\LaravelPackageMaker\Commands\Foundation\ModelMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\Package\GitignoreMakeCommand;
+use Naoray\LaravelPackageMaker\Commands\Foundation\PolicyMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\Database\MigrationMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\Routing\ControllerMakeCommand;
 use Naoray\LaravelPackageMaker\Commands\Foundation\ProviderMakeCommand;
@@ -36,40 +38,94 @@ class LaravelPackageMakerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->commands([
-            AddPackage::class,
-            JobMakeCommand::class,
-            TestMakeCommand::class,
-            MailMakeCommand::class,
-            RuleMakeCommand::class,
-            EventMakeCommand::class,
-            ModelMakeCommand::class,
-            ReadmeMakeCommand::class,
-            PolicyMakeCommand::class,
-            TravisMakeCommand::class,
-            PackageMakeCommand::class,
-            StyleciMakeCommand::class,
-            CodecovMakeCommand::class,
-            FactoryMakeCommand::class,
-            LicenseMakeCommand::class,
-            PhpunitMakeCommand::class,
-            ComposerMakeCommand::class,
-            BaseTestMakeCommand::class,
-            ProviderMakeCommand::class,
-            GitignoreMakeCommand::class,
-            MigrationMakeCommand::class,
-            ControllerMakeCommand::class,
-            ContributionMakeCommand::class,
-        ]);
+        $this->commands(
+            array_merge(
+                $this->packageInternalCommands(),
+                $this->databaseCommands(),
+                $this->foundationCommands(),
+                $this->packageCommands(),
+                $this->routingCommands()
+            )
+        );
     }
 
     /**
-     * Register the application services.
-     *
-     * @return void
+     * Get package internal related commands.
+     * 
+     * @return array
      */
-    public function register()
+    protected function packageInternalCommands()
     {
-        //
+        return [
+            AddPackage::class,
+            PackageMakeCommand::class,
+            SetPackageCredentials::class,
+            DeletePackageCredentials::class,
+        ];
+    }
+
+    /**
+     * Get package database related commands.
+     *
+     * @return array
+     */
+    protected function databaseCommands()
+    {
+        return [
+            FactoryMakeCommand::class,
+            MigrationMakeCommand::class,
+        ];
+    }
+
+    /**
+     * Get package foundation related commands.
+     *
+     * @return array
+     */
+    protected function foundationCommands()
+    {
+        return [
+            JobMakeCommand::class,
+            TestMakeCommand::class,
+            RuleMakeCommand::class,
+            MailMakeCommand::class,
+            EventMakeCommand::class,
+            ModelMakeCommand::class,
+            PolicyMakeCommand::class,
+            ProviderMakeCommand::class,
+        ];
+    }
+
+    /**
+     * Get package related commands.
+     *
+     * @return array
+     */
+    protected function packageCommands()
+    {
+        return [
+            TravisMakeCommand::class,
+            ReadmeMakeCommand::class,
+            StyleciMakeCommand::class,
+            LicenseMakeCommand::class,
+            PhpunitMakeCommand::class,
+            CodecovMakeCommand::class,
+            ComposerMakeCommand::class,
+            BaseTestMakeCommand::class,
+            GitignoreMakeCommand::class,
+            ContributionMakeCommand::class,
+        ];
+    }
+
+    /**
+     * Get package routing related commands.
+     *
+     * @return array
+     */
+    protected function routingCommands()
+    {
+        return [
+            ControllerMakeCommand::class,
+        ];
     }
 }
