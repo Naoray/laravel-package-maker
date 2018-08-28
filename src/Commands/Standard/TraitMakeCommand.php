4d220950d4649987f9a07106561925be03e9f6cd
Naoray <krishan.koenig@googlemail.com>
2018-08-28 22:00:51 +0200

    add & use AnyCommand

--- src/Commands/Standard/TraitMakeCommand.php
+++ src/Commands/Standard/TraitMakeCommand.php
@@ -4,9 +4,8 @@ namespace Naoray\LaravelPackageMaker\Commands\Standard;
 
 use Illuminate\Console\Command;
 use Naoray\LaravelPackageMaker\Traits\HasNameInput;
-use Naoray\LaravelPackageMaker\Commands\GeneratorCommand;
 
-class TraitMakeCommand extends GeneratorCommand
+class TraitMakeCommand extends Command
 {
     use HasNameInput;
 
@@ -38,47 +37,10 @@ class TraitMakeCommand extends GeneratorCommand
      */
     public function handle()
     {
-        parent::handle();
-
-        $name = $this->qualifyClass($this->getNameInput());
-
-        $path = $this->getPath($name);
-
-        $this->callSilent('package:replace', [
-            'path' => $path,
-            '--old' => ['DummyType'],
-            '--new' => ['trait'],
+        $this->callSilent('package:any', [
+            'name' => $this->argument('name'),
+            '--type' => 'trait',
+            '--category' => $this->type,
         ]);
-    }
-
-    /**
-     * Get the stub file for the generator.
-     *
-     * @return string
-     */
-    protected function getStub()
-    {
-        return __DIR__.'/stubs/dummy.stub';
-    }
-
-    /**
-     * Get the destination class path.
-     *
-     * @return string
-     */
-    protected function resolveDirectory()
-    {
-        return $this->getDirInput().'src';
-    }
-
-    /**
-     * Get the default namespace for the class.
-     *
-     * @param  string  $rootNamespace
-     * @return string
-     */
-    protected function getDefaultNamespace($rootNamespace)
-    {
-        return $rootNamespace.'\Traits';
     }
 }
