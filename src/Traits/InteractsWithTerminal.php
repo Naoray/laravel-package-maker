391fc118b90a3b4d333c2819449b6ee5d37a390d
Naoray <krishan.koenig@googlemail.com>
2018-08-28 21:25:53 +0200

    add trait to execute commands on terminal reliable

---
+++ src/Traits/InteractsWithTerminal.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Naoray\LaravelPackageMaker\Traits;
+
+use Symfony\Component\Process\Process;
+
+trait InteractsWithTerminal
+{
+	/**
+	 * Run the given command as a process.
+	 *
+	 * @param  string  $command
+	 * @param  string  $path
+	 * @return void
+	 */
+	protected function runCommand($command, $path)
+	{
+		$process = (new Process($command, $path))->setTimeout(null);
+
+		if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
+			$process->setTty(true);
+		}
+
+		$process->run(function ($type, $line) {
+			$this->command->output->write($line);
+		});
+	}
+}
